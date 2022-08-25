9b64f754bad43be13146f1ee2186926f23d2dee0
Sergey.Burlakov <sergey.burlakov@brandpulse.net>
2022-08-25 08:05:50 +0300

    using default file

--- src/test/java/ReaderControllerTest.java
+++ src/test/java/ReaderControllerTest.java
@@ -21,7 +21,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(classes = Application.class)
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
 public class ReaderControllerTest {
 
     @Autowired
