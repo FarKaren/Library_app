37e900601965356bda3e0b4384570c590fd8f328
Nataly Arkadeva <natatenok73@mail.ru>
2022-08-29 11:42:49 +0400

    Correct Reader personal data2 new

--- src/test/java/ReaderControllerTest.java
+++ src/test/java/ReaderControllerTest.java
@@ -1,14 +1,12 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.community.Application;
-import ru.community.dto.ReaderEditDto;
 import ru.community.entity.Reader;
 import ru.community.repository.ReaderRepository;
 
@@ -32,6 +30,12 @@ public class ReaderControllerTest {
     @Autowired
     private ReaderRepository repository;
 
+    @BeforeEach
+    public void deleteAll() {
+        repository.deleteAll();
+    }
+
+
     @Test
     public void getReaderByIdTest() throws Exception {
         var reader = new Reader(1, "Аркадий", "Павлов", "8-999-124-45-54", "qwe@.fgk.ty",
@@ -43,27 +47,27 @@ public class ReaderControllerTest {
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("id").value(1));
-//                .andExpect(jsonPath("name").value("Аркадий"));
+                .andExpect(jsonPath("id").value(1))
+                .andExpect(jsonPath("name").value("Аркадий"));
     }
 
     @Test
     public void editReaderTest() throws Exception {
-        var reader = new Reader(-6, "Григорий", "Архипов", "8-999-299-88-77", "qwerty@mail.ru",
+        var reader = new Reader(1, "Григорий", "Архипов", "8-999-299-88-77", "qwerty@mail.ru",
                 LocalDate.of(1985, 11, 25));
         repository.save(reader);
 
-        var readerEditDto = ReaderEditDto.builder()
-                .phoneNumber("8-999-299-88-11")
-                .dateOfBirth(LocalDate.of(1985, 1, 25))
-                .build();
+        var readerEdit = new Reader(1, "Александр", "Петров", "8-555-555-55-55", "qwerty@gmail.com",
+                LocalDate.of(1999, 1, 15));
 
         this.mockMvc.perform(
-                        put("/reader/{id}/me/edit", -6)
+                        put("/reader/{id}/me/edit", reader.getId())
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .accept(MediaType.APPLICATION_JSON)
-                                .content(mapper.writeValueAsString(readerEditDto)))
+                                .content(mapper.writeValueAsString(readerEdit)))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("phoneNumber").value("8-999-299-88-11"));
+                .andExpect(jsonPath("phoneNumber").value("8-555-555-55-55"))
+                .andExpect(jsonPath("name").value("Григорий"))
+                .andExpect(jsonPath("surname").value("Архипов"));
     }
 }
